66b9fc90ceb55f54bf9d11eeb48cea83f99285b2
mZubeldia <mzubeldia09@gmail.com>
2021-07-19 16:05:23 +0200

    add script fontawesome

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -10,7 +10,10 @@ const CharacterCard = (props) => {
           alt={props.characterData.name}
         />
 
-        <h2 className="character-name">{props.characterData.name}</h2>
+        <h2 className="character-name">
+          {props.characterData.name}{" "}
+          <i class="fab fa-reddit-alien species-icon"></i>
+        </h2>
         <p className="character-species">{props.characterData.species}</p>
       </article>
     </Link>
